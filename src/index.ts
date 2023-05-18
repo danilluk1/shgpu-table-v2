8350cbc844acd660776e8632eb97a7c2bbfc2f83
danluki <danluki02@yandex.ru>
2023-05-18 15:01:59 +0500

    feat: project structure

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import "reflect-metadata";
+import "source-map-support/register";
+
+console.log("Hello world!");
+// import { AppDataSource }
+
+// AppDataSource.init();
+
+// parser = new parser();
+// const tableInfo = parser.parseTable("linktotable")
+// console.log(tableInfo);

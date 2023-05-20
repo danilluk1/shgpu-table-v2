d583a3164a04f74a6a62067c8aa0f9f2ba40899c
Phoboss <102321702+Phoboss33@users.noreply.github.com>
2023-05-21 00:33:12 +0500

    make next week and tomorrow buttons

--- src/services/telegram/composers/find-pairs.ts
+++ src/services/telegram/composers/find-pairs.ts
@@ -1,7 +1,9 @@
 import { Composer } from "grammy";
 import { Context } from "../types";
-import { getPairsForWeekCommand} from "../../../commands/get-pairs-weekly";
-import { getPairsForDayCommand} from "../../../commands/get-pairs-today";
+import { getPairsForWeekCommand } from "../../../commands/get-pairs-weekly";
+import { getPairsForDayCommand } from "../../../commands/get-pairs-today";
+import { getPairsForTomorrowCommand } from "../../../commands/get-pairs-tommorow";
+import { getPairsForNextWeekCommand } from "../../../commands/get-pairs-next-week";
 import { TelegramMessageSender } from "../message-sender";
 
 export const findPairsComposer = new Composer<Context>();
@@ -52,6 +54,48 @@ findPairsComposer.hears(/Пары сегодня/i, async (ctx) => {
   }
 
   const { messages } = await getPairsForDayCommand(ctx.session.sub);
+  for (const message of messages) {
+    await TelegramMessageSender.sendMessage({
+      target: ctx.chat.id,
+      message: message,
+    });
+  }
+});
+
+findPairsComposer.hears(/Пары на след неделю/i, async (ctx) => {
+  let msg = "";
+  if (!ctx.session.sub) {
+    msg =
+      "Сначала подпишитесь на одну из групп, чтобы использовать короткую команду.";
+    await TelegramMessageSender.sendMessage({
+      target: ctx.chat.id,
+      message: msg,
+    });
+    return;
+  }
+
+  const { messages } = await getPairsForNextWeekCommand(ctx.session.sub);
+  for (const message of messages) {
+    await TelegramMessageSender.sendMessage({
+      target: ctx.chat.id,
+      message: message,
+    });
+  }
+});
+
+findPairsComposer.hears(/Пары завтра/i, async (ctx) => {
+  let msg = "";
+  if (!ctx.session.sub) {
+    msg =
+      "Сначала подпишитесь на одну из групп, чтобы использовать короткую команду.";
+    await TelegramMessageSender.sendMessage({
+      target: ctx.chat.id,
+      message: msg,
+    });
+    return;
+  }
+
+  const { messages } = await getPairsForTomorrowCommand(ctx.session.sub);
   for (const message of messages) {
     await TelegramMessageSender.sendMessage({
       target: ctx.chat.id,

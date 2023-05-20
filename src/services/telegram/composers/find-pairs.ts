929a6717aa360d49cade702f4a23d7e34dbd3bda
Phoboss <102321702+Phoboss33@users.noreply.github.com>
2023-05-20 19:24:40 +0500

    add pair-today

--- src/services/telegram/composers/find-pairs.ts
+++ src/services/telegram/composers/find-pairs.ts
@@ -1,6 +1,7 @@
 import { Composer } from "grammy";
 import { Context } from "../types";
-import { getPairsForWeekCommand } from "../../../commands/get-pairs-weekly";
+import { getPairsForWeekCommand} from "../../../commands/get-pairs-weekly";
+import { getPairsForDayCommand} from "../../../commands/get-pairs-today";
 import { TelegramMessageSender } from "../message-sender";
 
 export const findPairsComposer = new Composer<Context>();
@@ -30,6 +31,27 @@ findPairsComposer.hears(/Пары \S+ на неделю/i, async (ctx) => {
   const groupName = ctx.message.text.split(" ")[1];
 
   const { messages } = await getPairsForWeekCommand(ctx.session.sub, groupName);
+  for (const message of messages) {
+    await TelegramMessageSender.sendMessage({
+      target: ctx.chat.id,
+      message: message,
+    });
+  }
+});
+
+findPairsComposer.hears(/Пары сегодня/i, async (ctx) => {
+  let msg = "";
+  if (!ctx.session.sub) {
+    msg =
+      "Сначала подпишитесь на одну из групп, чтобы использовать короткую команду.";
+    await TelegramMessageSender.sendMessage({
+      target: ctx.chat.id,
+      message: msg,
+    });
+    return;
+  }
+
+  const { messages } = await getPairsForDayCommand(ctx.session.sub);
   for (const message of messages) {
     await TelegramMessageSender.sendMessage({
       target: ctx.chat.id,

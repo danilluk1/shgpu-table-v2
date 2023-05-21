3fd749d71d14d5078c9f8c726e4e753470b6253c
danluki <danluki02@yandex.ru>
2023-05-21 18:48:37 +0500

    fix: just for testing

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -3,7 +3,7 @@ import { Keyboard } from "grammy";
 export const startCommand = () => {
   return {
     success: true,
-    message: `Добро пожаловать в неофициального бота расписания ШГПУ`,
+    message: `Добро пожаловать в неофициального бота расписания ШГПУ.`,
     kb: new Keyboard([
       ["Пары на неделю", "Пары на след неделю"],
       ["Пары завтра", "Пары сегодня"],
